f9f35b15eabf541af30dcc776c1f75ec4f8fee4b
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-04-19 23:09:23 -0400

    align the text insie the smurf box

--- src/components/Smurf.js
+++ src/components/Smurf.js
@@ -4,9 +4,9 @@ const Smurf = props => {
 
     return (
         <div className="smurf">
-            <h3>Name: </h3><span>{props.smurf.name}</span>
-            <h3>Age: </h3><span>{props.smurf.age}</span>
-            <h3>Height: </h3><span>{props.smurf.height}</span>
+            <h3>Name: <span>{props.smurf.name}</span></h3>
+            <h3>Age: <span>{props.smurf.age}</span></h3>
+            <h3>Height: <span>{props.smurf.height}</span></h3>
         </div>
     )
 }
