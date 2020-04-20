3385c23a16df1decdaf0d950849a1df7b1c9556d
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-04-19 22:50:32 -0400

    added form to create smurf and completed MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import SmurfListView from './components/SmurfListView';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <h3>Smurfs 2020</h3>
+      <br />
+      <Form />
       <br />
       <SmurfListView />
     </div>
---
+++ src/components/Form.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { addSmurf } from '../redux/actions/actions'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+
+class Form extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            name: '',
+            age: '',
+            height: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    createSmurf = (e) => {
+        e.preventDefault()
+        const { name, age, height } = this.state
+        this.props.addSmurf(name, age, height)
+            .catch((err => {
+                console.log(err)
+            }))
+        this.setState = {
+            name: '',
+            age: '',
+            height: ''
+        }
+    }
+
+    render() {
+        const { name, age, height } = this.state
+        const { fetchingSmurf, error } = this.state
+
+        return (
+            <div className="form-body">
+                <form
+                    className="form"
+                    onSubmit={this.createSmurf}
+                >
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        value={name}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="age"
+                        placeholder="Age"
+                        value={age}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="height"
+                        placeholder="Height"
+                        value={height}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <br />
+                    {fetchingSmurf ? <p>Loading...</p> : <button type="submit">Create Smurf</button>}
+                </form>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = {
+    addSmurf
+}
+
+const mapStateToProps = (state) => ({
+    fetchingSmurfs: state.fetchingSmurfs,
+    error: state.error
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Form))
